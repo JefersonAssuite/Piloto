8d01b204c41a50f3ce811582bd7b5449c9ac6934
Jeferson Assuite <86379148+JefersonAssuite@users.noreply.github.com>
2025-01-08 09:06:00 -0300

    Update FirebaseConfig.js

--- services/FirebaseConfig.js
+++ services/FirebaseConfig.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBEahurTA0ui_Xq8TtB0q0iJey0strv67U",
-    authDomain: "feed-56b79.firebaseapp.com",
-    projectId: "feed-56b79",
-    storageBucket: "feed-56b79.firebasestorage.app",
-    messagingSenderId: "557777012202",
-    appId: "1:557777012202:web:4de61aab5a4ada4f8ede93",
-    measurementId: "G-CXV7BCTN1C"
+    apiKey: "######
+    authDomain: "##
+    projectId: "#
+    storageBucket: #
+    messagingSenderId: ###
+    appId: "###
+    measurementId: "3###
   };
 
   const app = initializeApp(firebaseConfig);
